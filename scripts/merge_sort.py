966f913ffc4d28d8bfb200bb12c118c0d71e906f
Athiyamaan Baskar <athi@macbook-pro.lan>
2021-05-11 17:10:03 -0500

    changes

---
+++ scripts/merge_sort.py
@@ -0,0 +1,22 @@
+# Merge two list(already sorted), final list should be sorted.
+
+def merge(l, m):
+    result = []
+    i = j = 0
+    total = len(l) + len(m)
+    while len(result) != total:
+        if len(l) == i:
+            result += m[j:]
+            break
+        elif len(m) == j:
+            result += l[i:]
+            break
+        elif l[i] < m[j]:
+            result.append(l[i])
+            i += 1
+        else:
+            result.append(m[j])
+            j += 1
+    return result
+
+print(merge([1,2,6,7], [1,2,5,9]))
